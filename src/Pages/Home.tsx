3dc276c22a76cadf87a926741f04d3e26e5fe419
Anderson-Lai <andersonmhlai26@gmail.com>
2024-07-31 22:12:32 -0400

    refactoring home

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -26,21 +26,27 @@ export default function Home() {
                 <h4 className="font-semibold mt-10">Rising Artists Marketing and Storytelling</h4>
 
                 <button className="mt-12 text-2xl rounded-xl border border-border-light-yellow px-3 py-2
-                 hover:-translate-y-2 duration-200 delay-100 hover:scale-110 hover:shadow-white">
+                 hover:-translate-y-1 duration-200 delay-100 hover:scale-105 hover:shadow-white">
                     <Link to="/contact">Contact Us</Link>
                 </button>
             </div>
 
             <div className="mt-14 lg:mx-24 lg:block flex flex-col items-center">
                 <h3 className="font-semibold text-3xl mb-8">About Us</h3>
-                <div className="grid grid-cols-1 lg:grid-cols-2 place-items-center lg:place-items-start">
-                    <div className="flex flex-col lg:items-start items-center">
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                    <div className="flex justify-center lg:justify-start">
                         <Card image={cardData.purpose.image} title="Our Purpose" body={cardData.purpose.body} />
+                    </div>
+                    <div className="flex justify-center lg:justify-end">
+                        <Card image={cardData.announcements.image} title="Daily Announcements" body={cardData.announcements.body} />
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 lg:grid-cols-2">
+                    <div className="flex justify-center lg:justify-start">
                         <Card image={cardData.goals.image} title="Our Goals" body={cardData.goals.body} />
                     </div>
-
-                    <div className="flex flex-col lg:items-end items-center">
-                        <Card image={cardData.announcements.image} title="Daily Announcements" body={cardData.announcements.body} />
+                    <div className="flex justify-center lg:justify-end">
                         <Card image={cardData.music.image} title="Morning Music" body={cardData.music.body} />
                     </div>
                 </div>
@@ -51,7 +57,7 @@ export default function Home() {
                 <p className="text-center my-9" style={{maxWidth: "50%"}}>{appDescription}</p>
 
                 <button className="mt-4 text-2xl rounded-xl border border-border-light-yellow px-3 py-2
-                 hover:-translate-y-2 duration-200 delay-100 hover:scale-110 hover:shadow-white"
+                 hover:-translate-y-1 duration-200 delay-100 hover:scale-105 hover:shadow-white"
                  onClick={handleDownload}>
                     Download the App
                 </button>
