6412ead967890875d4d4a0018cd9f6729c07a6d2
Anderson-Lai <andersonmhlai26@gmail.com>
2024-09-03 16:58:44 -0400

    fix image bug

--- src/Pages/Resources.tsx
+++ src/Pages/Resources.tsx
@@ -17,8 +17,7 @@ export default function Resources() {
                         Object.keys(generalData).map(k => {
                             const key = k as keyof typeof generalData;
                             return (
-                                <Card image={generalData[key].image} title={generalData[key].title}
-                                body={generalData[key].body} link={generalData[key].link} key={v4()} />
+                                <Card {...generalData[key]} key={v4()} />
                             );
                         })
                     }
@@ -29,30 +28,21 @@ export default function Resources() {
                 <h3 className="font-semibold text-3xl mb-8">Guidance Resources</h3>
 
                 <div className="grid grid-cols-1 lg:grid-cols-3 lg:gap-x-7 place-items-stretch">
-                    {/* {
-                        Object.keys(guidanceData).map((k) => {
-                            const key = k as keyof typeof guidanceData;
-                            return (
-                                <Card image={guidanceData[key].image} title={guidanceData[key].title}
-                                body={guidanceData[key].body} link={guidanceData[key].link} key={v4()} />
-                            );
-                        })
-                    } */}
-                    <Card image={guidanceData.appointment.image} title="Book an appointment" body={guidanceData.appointment.body}
+                    <Card title="Book an appointment" body={guidanceData.appointment.body}
                     link={guidanceData.appointment.link} />
 
-                    <Card image={guidanceData.courseChange.image} title="Course changes" body={guidanceData.courseChange.body}
+                    <Card title="Course changes" body={guidanceData.courseChange.body}
                     link={guidanceData.courseChange.link} />
 
-                    <Card image={guidanceData.guidanceWebsite.image} title="Guidance website" body={guidanceData.guidanceWebsite.body}
+                    <Card title="Guidance website" body={guidanceData.guidanceWebsite.body}
                     link={guidanceData.guidanceWebsite.link} />
                 </div>
 
                 <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-7">
-                    <Card image={guidanceData.shsm.image} title="SHSM" body={guidanceData.shsm.body}
+                    <Card title="SHSM" body={guidanceData.shsm.body}
                     link={guidanceData.shsm.link} />
 
-                    <Card image={guidanceData.tutoring.image} title="Tutoring" body={guidanceData.tutoring.body}
+                    <Card title="Tutoring" body={guidanceData.tutoring.body}
                     link={guidanceData.tutoring.link} />
                 </div>
             </div>
