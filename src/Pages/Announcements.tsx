9212e0f196c736f0f402b6571d73b6a18ea33baf
Anderson-Lai <andersonmhlai26@gmail.com>
2024-10-02 10:01:48 -0400

    announcement embed proposition

--- src/Pages/Announcements.tsx
+++ src/Pages/Announcements.tsx
@@ -9,14 +9,13 @@ export default function Announcements() {
             <div className="my-14 flex justify-center px-2">
                 <AnnouncementCard
                     title="View the latest announcements!"
-                    link="https://docs.google.com/spreadsheets/d/1ls3lAzoKo3OTLW5JOCwc21LVKx-p0PCFeidrfXkptME/edit?usp=sharing"
-                    // embed={
-                    //     <iframe
-                    //     src="https://docs.google.com/spreadsheets/d/1ZHHIM4D76i2zmnLPxDSmiLLpQkJtFA_D-8xZSo1T8EY/pubhtml?widget=true&amp;headers=false"
-                    //         style={{ border: 'none', width: '100%', height: '100%' }}
-                    //         title="Announcement Spreadsheet"
-                    //     />
-                    // }
+                    link="https://docs.google.com/spreadsheets/d/120NMj5KKX5_XBEjrG6iJCzZ9LUcZALpSd0bEZTqQveQ/edit?usp=sharing"
+                    embed={
+                        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRt19P0ItVrsvrc9wz8xvn2_vqrGDKdD6Qp5ADFxVeIiV69Irp6TORebYDwI2gSOmEGsevsWFPCggI4/pubhtml?widget=true&amp;headers=false"
+                            style={{ width: '100%', height: '100%' }}
+                            title="Announcement Spreadsheet"
+                        ></iframe>
+                    }
                 />
             </div>
         </>
