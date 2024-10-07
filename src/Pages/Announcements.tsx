ee4a149f12009c950d8b2499847303ddec2d91f2
Anderson-Lai <andersonmhlai26@gmail.com>
2024-10-07 11:04:39 -0400

    update spreadsheet link

--- src/Pages/Announcements.tsx
+++ src/Pages/Announcements.tsx
@@ -9,7 +9,7 @@ export default function Announcements() {
             <div className="my-14 flex justify-center px-2">
                 <AnnouncementCard
                     title="View the latest announcements!"
-                    link="https://docs.google.com/spreadsheets/d/1ls3lAzoKo3OTLW5JOCwc21LVKx-p0PCFeidrfXkptME/edit?usp=sharing"
+                    link="https://docs.google.com/spreadsheets/d/120NMj5KKX5_XBEjrG6iJCzZ9LUcZALpSd0bEZTqQveQ/edit?usp=sharing"
                     // embed={
                     //     <iframe
                     //     src="https://docs.google.com/spreadsheets/d/1ZHHIM4D76i2zmnLPxDSmiLLpQkJtFA_D-8xZSo1T8EY/pubhtml?widget=true&amp;headers=false"
