6c8f862b28831b46a162713778192acc33530dfd
Anderson-Lai <andersonmhlai26@gmail.com>
2024-10-30 16:11:15 -0400

    media of the month

--- src/Data/ShowreelData.tsx
+++ src/Data/ShowreelData.tsx
@@ -2,6 +2,14 @@ import type { ShowreelCardProps } from "../Components/ShowreelCard";
 import example from "../Videos/example.mp4"
 import autreExample from "../Videos/health.mp4"
 
+export const mediaOfTheMonth: ShowreelCardProps = {
+    media: example,
+    title: "Interpretive Line Dancing",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima voluptatibus aliquid blanditiis officiis quos, non voluptatem quis aperiam, accusamus optio voluptates deleniti commodi. Ducimus atque laboriosam modi. Eos, assumenda! Quae.",
+    author: "Some Really Creative Person",
+    alignVideo: "left"
+}
+
 export const showreelElements: ShowreelCardProps[] = [
     {
         media: example,
--- src/Pages/Showreel.tsx
+++ src/Pages/Showreel.tsx
@@ -1,14 +1,36 @@
 import { v4 } from "uuid";
 import PageTitle from "../Components/PageTitle";
 import ShowreelCard from "../Components/ShowreelCard";
-import { showreelElements } from "../Data/ShowreelData";
+import { showreelElements, mediaOfTheMonth } from "../Data/ShowreelData";
+
+type HeaderProps = {
+    heading: string
+}
+
+function Header( { heading } : HeaderProps ) {
+    return (
+        <h2 className="font-semibold text-3xl text-center">{heading}</h2>
+    );
+}
 
 export default function Showreel() {
     return (
         <>
-            <PageTitle title="SHOWREEL" />
-            
-            <div className="mt-24 mx-5 lg:mx-24 flex flex-col justify-center items-center space-y-20">
+            <div className="mb-24">
+                <PageTitle title="SHOWREEL" />
+            </div>
+        
+            <Header heading="Media of the Month" />
+            <div className="mt-12 mx-5 lg:mx-24">
+
+                <ShowreelCard {...mediaOfTheMonth} />
+            </div>
+
+            <div className="mt-24">
+                <Header heading="Media By Your Executives" />
+            </div>
+
+            <div className="mt-12 mx-5 lg:mx-24 flex flex-col justify-center items-center space-y-20">
                 {
                     showreelElements.map((s) => {
                         return <ShowreelCard {...s} key={v4()} />
