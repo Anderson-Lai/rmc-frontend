8b130ca2cc2cb5e146c210d4ca2135d9089ac839
Anderson-Lai <andersonmhlai26@gmail.com>
2024-10-30 21:35:59 -0400

    const correctness

--- src/Pages/Extracurriculars.tsx
+++ src/Pages/Extracurriculars.tsx
@@ -7,7 +7,7 @@ import { v4 } from "uuid";
 
 function isAlphaNumeric(string: string) {
     for (let i = 0; i < string.length; i++) {
-        let code = string.charCodeAt(i);
+        const code = string.charCodeAt(i);
         if ((code >= 48 && code <= 57) ||
             (code >= 65 && code <= 90) ||
             (code >= 97 && code <= 122) ||
